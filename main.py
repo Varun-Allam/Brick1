2e2b2402c481b614bf7e872352bb095e14b80d84
Varun-Allam <145447037+Varun-Allam@users.noreply.github.com>
2025-09-01 23:25:34 +0200

    Update main.py

--- main.py
+++ main.py
@@ -3,5 +3,4 @@ import eel
 
 eel.init("www") 
 
-os.system('start local.exe --app="http://localhost:800/index.html"') 
-eel.start('index.html',mode=None,host='localhost',block=True)eel.start('index.html', mode="chrome", host='localhost', block=True)eel.start('index.html', mode="chrome", host='localhost', block=True)
+eel.start('index.html', mode="chrome", host='localhost', block=True)
